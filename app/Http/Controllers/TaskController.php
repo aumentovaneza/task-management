19a8bbc02e459ff363ddee920cbb9f88a2c04c88
vaneza <vaneza.aumento@gmail.com>
2023-01-13 14:50:51 +0800

    updated migration and model, added resource and controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
--- database/migrations/2023_01_13_062924_create_tasks_table.php
+++ database/migrations/2023_01_13_062924_create_tasks_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('name');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
